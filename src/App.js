2c57197a230dc01342f80e8d044228413d92e0a6
vbhv007 <bhardwajvbhv@gmail.com>
2020-04-16 23:34:32 +0530

    mapbox setup complete, bangalore map is loaded

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import ReactMapGL from 'react-map-gl';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+const App = () => {
+    const [viewport, setViewport] = useState({
+        width:  400,
+        height: 600,
+        latitude:   12.9716,
+        longitude:  77.5946,
+        zoom:   8
+    });
+
+    return (
+        <ReactMapGL
+            {...viewport}
+            mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+            onViewportChange={setViewport}
+        />
+    );
 }
+
 
 export default App;
