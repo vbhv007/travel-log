232019b6d6b3e81ab5b75aecace8c8de34c91945
vbhv007 <bhardwajvbhv@gmail.com>
2020-04-17 23:57:20 +0530

    added api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactMapGL from 'react-map-gl';
+
+import { listAllLogs } from './api';
 
 const App = () => {
     const [viewport, setViewport] = useState({
-        width:  400,
-        height: 600,
-        latitude:   12.9716,
-        longitude:  77.5946,
-        zoom:   8
+        width:  '100vw',
+        height: '100vh',
+        latitude:   20.5937,
+        longitude:  78.9629,
+        zoom:   4
     });
+
+    useEffect(() => {
+        (async () => {
+            const logs = await listAllLogs();
+            console.log(logs);
+        })();
+    }, []);
 
     return (
         <ReactMapGL
---
+++ src/api.js
@@ -0,0 +1,10 @@
+const API_URL = "http://localhost:8080"
+
+
+export async function listAllLogs() {
+    const response = await fetch(API_URL + "/logs", {
+        method: "POST",
+        body: JSON.stringify({}),
+    });
+    return response.json()
+}
